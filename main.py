97d811d93552793d6bbed93eb98b706c55665138
Theooking <nikosav2007@gmail.com>
2024-07-13 23:45:54 +0300

    better main.py, .kv file needs configuring

--- main.py
+++ main.py
@@ -2,32 +2,63 @@ import kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.stacklayout import StackLayout
-from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 
 kivy.require("2.3.0")
+
+class ProductButton(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.orientation = "vertical"
+        self.size_hint = (None, None)
+        self.size = (150, 150)
+
+        self.add_widget(
+            Label(text="img")
+        )
+
+        self.add_widget(
+            Label(text="info")
+        )
+
+        self.add_widget(
+            Button(text="Add to cart")
+        )
 
 
 class Products(StackLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.orientation = 'lr-tb'
+        self.padding = 10
+        self.spacing = 10
 
-        for i in range(1,26):
+        for i in range(1, 26):
             self.add_widget(
-                Button(text=f"{1}", size_hint=(.2,.2))
+                ProductButton()
             )
+
 
 class Menu(BoxLayout): 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.orientation = "vertical"
+
+        header = BoxLayout(size_hint=(1, 0.1))
+        header.add_widget(Button(text="Settings"))
+        header.add_widget(Label(text="ShopPal"))
+        header.add_widget(Button(text="Cart"))
+        self.add_widget(header)
+
         self.add_widget(Products())
 
 
 class ShopPalApp(App):
-    def build(self) -> None:
+    def build(self):
         return Menu()
     
     
-shopPal = ShopPalApp()
-shopPal.run()
+if __name__ == '__main__':
+    ShopPalApp().run()
